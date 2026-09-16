240fc1ac9ac69f7ac1c610605ef91a097d9b6681
zeroxounder <106141080+zeroxounder@users.noreply.github.com>
2026-09-16 12:54:41 +0800

    Update static.ts

--- src/static.ts
+++ src/static.ts
@@ -3,7 +3,7 @@ import path from "path"
 import { buildFeed } from "./feed.js"
 import { initCache } from "./cache.js"
 
-const BASE_URL = "https://zeroxounder.github.io/raiplaysound-feed"
+const BASE_URL = "https://giuliomagnifico.github.io/raiplaysound-feed"
 
 const podcasts = [
   {
